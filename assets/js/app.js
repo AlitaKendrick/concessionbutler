30ae8aa4c6c3d1c12f46848c9a45ebaf7302bed7
Benjamin Williams <ben@benjaminwilliams.com>
2017-03-13 21:19:43 -0500

    Initialize Firebase

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+ var config = {
+    apiKey: "AIzaSyDPpEKP-rTcpgLVZk2j18ZLAfgwQhriTxI",
+    authDomain: "concession-butler.firebaseapp.com",
+    databaseURL: "https://concession-butler.firebaseio.com",
+    storageBucket: "concession-butler.appspot.com",
+    messagingSenderId: "268811474889"
+  };
+  firebase.initializeApp(config);
