2d60788186148214d0811f235b2a92376dbaf7d4
Benjamin Williams <ben@benjaminwilliams.com>
2017-03-15 20:07:32 -0500

    Firebase pushing

--- assets/js/create-order.js
+++ assets/js/create-order.js
@@ -9,7 +9,7 @@
 firebase.initializeApp(config);
 
 var server = null;
-
+var database = firebase.database();
 
 var menu = [
 	{
@@ -27,7 +27,9 @@ var menu = [
 //	return 'You will lose any unsaved changes you may have made';
 //}
 
-
+/*
+Clicks
+*/
 $(document).on('click', '#setServer', function() {
 
 	event.preventDefault();
@@ -40,12 +42,41 @@ $(document).on('click', '#setServer', function() {
 		//hide current visible window
 		$('.serverSelection').remove();
 	}
+	
 	else{
 
 		console.log("not set");
 
 	}
 	
-	    	
 });
 
+//Actual submit
+$(document).on('click', '#setServer', function() {
+	
+	database.ref().push({
+
+	        order: "order",
+	        server: server,
+	        status: "active",
+	        dateAdded: firebase.database.ServerValue.TIMESTAMP
+	});
+
+});
+
+
+/*
+Testing
+*/
+
+function pushToDb(){
+	
+	database.ref().push({
+
+	        order: "order",
+	        server: server,
+	        status: "active",
+	        dateAdded: firebase.database.ServerValue.TIMESTAMP
+	});
+
+};
